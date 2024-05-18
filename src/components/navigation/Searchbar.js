20787920ba58a815e5e5e95fcc9f5752be3af535
VISHAL KUMAR SINHA <vishalsinha211299@gmail.com>
2024-05-18 07:55:30 +0530

    aa

--- src/components/navigation/Searchbar.js
+++ src/components/navigation/Searchbar.js
@@ -74,6 +74,7 @@ const Searchbar = () => {
                   id="floating_search"
                   className="block pt-2.5 px-0 w-full text-md text-dark bg-transparent border-0 border-b-2 border-dark appearance-none focus:outline-none focus:ring-0 focus:border-accent peer"
                   placeholder=" "
+				  autofocus
                   onChange={submitSearch}
                 />
                 <label className="peer-focus:font-bold absolute font-medium text-md text-dark duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-accent peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
